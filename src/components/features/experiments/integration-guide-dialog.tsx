5973605b8dd1e7c4af42a49a851cc25cb36427e0
qixotic <854794+qixotic@users.noreply.github.com>
2026-06-17 11:47:15 +0800

    add some missing explicitly used dependencies

--- src/components/features/experiments/integration-guide-dialog.tsx
+++ src/components/features/experiments/integration-guide-dialog.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Button, Callout, Card, DataList, Dialog, Flex, Select, Text } from '@radix-ui/themes';
 import { ChevronDownIcon, ChevronRightIcon, FileIcon, GearIcon, PlusIcon } from '@radix-ui/react-icons';
-import { Collapsible } from 'radix-ui';
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { useState } from 'react';
 import { mutate } from 'swr';
 import { Arm, Context } from '@/api/methods.schemas';
